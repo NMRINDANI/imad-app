afbca64955457a683ae79a6fce8fdfd3e5e28615
NMRINDANI <36433886+NMRINDANI@users.noreply.github.com>
2018-02-22 12:14:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,13 +24,17 @@ app.get('/test-db',function(req,res){
     //make a select request
     //return a response with the results
 });
+
+
 pool.query('SELECT * FROM test',function(err,result){
-    if(err){
+   
+   if(err)
+   {
         res.status(500).send(err.toString());
     }
+   
     else   {res.send(JSON.stringiFy(result));
-    }
-});
+            }
 });
 
 
