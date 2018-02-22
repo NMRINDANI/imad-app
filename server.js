21c9d3d7c8c9eae3ac60a09f2ff3f27a2a9e67a5
NMRINDANI <36433886+NMRINDANI@users.noreply.github.com>
2018-02-22 12:24:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var pool=require('pg').Pool;
 var config ={
-    user:'nmrindnai',
+    user:'nmrindani',
     database:'nmrindani',
     host:'db.imad.hasura-app.io',
     port:'5432',
