f5eed292ff49ceed9aa94f38b07473e261154dd1
NMRINDANI <36433886+NMRINDANI@users.noreply.github.com>
2018-02-23 11:31:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(morgan('combined'));
 
 
 //------------creating Tempplate-----funciton+articles
-/*function createTemplate(data){
+function createTemplate(data){
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
@@ -42,7 +42,7 @@ var htmlTemplate = `
 `;
 return htmlTemplate;
 }
-*/
+
 
 var articles = {
     
