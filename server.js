486e7d10b8d36a2a1bb89ee8d0ab71df341456af
NMRINDANI <36433886+NMRINDANI@users.noreply.github.com>
2018-03-05 12:51:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ function hash(input,salt){
     return hashed.toString('hex');
 }
 
-apt.get('/hash/:input', function(req,res){
+app.get('/hash/:input', function(req,res){
     var hashedString=hash(req.params.input,"this is ramdom string");
     res.send(hashedString);
     });
